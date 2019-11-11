9a668a40348905ac37dabc16b641d16236660645
Zhassulan Aimukhambetov <aimzhas@gmail.com>
2019-11-11 17:04:39 +0600

    Initial Commit

---
+++ lesson3.1/Model/SoundEffect.swift
@@ -0,0 +1,14 @@
+//
+//  SoundEffect.swift
+//  lesson3.1
+//
+//  Created by Zhassulan Aimukhambetov on 9/25/19.
+//  Copyright © 2019 Zhassulan Aimukhambetov. All rights reserved.
+//
+
+import Foundation
+import SwiftySound
+
+//class SoundEffect {
+//    let sound: Sound?
+//}
